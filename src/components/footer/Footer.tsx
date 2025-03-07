59cef5cf2fdb649a44d7731cbefd713bfc9b6f65
jloliveira95 <jl.oliveira95@gmail.com>
2025-03-07 01:41:52 -0300

    criação do footer, navbar e rotas

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,24 @@
+import { FacebookLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
+
+function Footer() {
+
+    let data = new Date().getFullYear()
+
+    return (
+        <>
+            <div className='flex gap-2'>
+                <a href="https://www.linkedin.com/in/seu_usuario" target="_blank">
+                    <LinkedinLogo size={48} weight='bold' />
+                </a>
+                <a href="https://www.instagram.com/seu_usuario" target="_blank">
+                    <InstagramLogo size={48} weight='bold' />
+                </a>
+                <a href="https://www.facebook.com/seu_usuario" target="_blank">
+                    <FacebookLogo size={48} weight='bold' />
+                </a>
+            </div>
+        </>
+    )
+}
+
+export default Footer
