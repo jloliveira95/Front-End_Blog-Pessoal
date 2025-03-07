50c456fd9175f2f2351c9b2a508f2b0df5b553db
jloliveira95 <jl.oliveira95@gmail.com>
2025-03-07 03:08:28 -0300

    implementando cadastro, models, services e login

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home'
-import './App.css'
+import Login from './pages/login/Login'
+import Cadastro from './pages/cadastro/Cadastro'
+import ListarUsuarios from './components/usuarios/listarusuarios/ListarUsuarios'
 
 function App() {
   return (
-    <>
-        <BrowserRouter>
-          <Navbar />
-          <div className="min-h-[80vh]">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/home" element={<Home />} />
-            </Routes>
-          </div>
-          <Footer />
-        </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <div className="min-h-[80vh]">
+        <Routes>
+          <Route index element={<Login />} /> {/* Define Login como página inicial */}
+          <Route path="/home" element={<Home />} />
+          <Route path="/cadastro" element={<Cadastro />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/usuarios" element={<ListarUsuarios />} /> {/* Agora acessível por rota */}
+        </Routes>
+      </div>
+      <Footer />
+    </BrowserRouter>
   )
 }
 
